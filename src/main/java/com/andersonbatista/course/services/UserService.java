95e4b73e671f335094d903d4bf66f7063e8eab6f
Anderson Batista Ferreira da Silva <andersonbfs15@gmail.com>
2021-04-24 08:49:45 -0300

    Exception handling - findById

--- src/main/java/com/andersonbatista/course/services/UserService.java
+++ src/main/java/com/andersonbatista/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.andersonbatista.course.entities.User;
 import com.andersonbatista.course.repositories.UserRepository;
+import com.andersonbatista.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findByid(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
